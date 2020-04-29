d3169be63f3fb94abfd49aa212d871fd14a91cea
JPedro Brito <jpedro0brito@gmail.com>
2020-04-28 23:27:17 -0400

    Tirando coisa desnecesaria

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 
 import App from './Pages/Home';
 import Sobre from './Pages/Sobre';
